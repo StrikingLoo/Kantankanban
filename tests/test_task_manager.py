299f3503d0b1228f3429592d8ae98a3fe3d55af0
strikingLoo <lucianostrika44@gmail.com>
2023-08-18 01:27:00 -0700

    fix tests

--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -19,7 +19,7 @@ def test_version():
 
 @pytest.fixture
 def mock_json_file(tmp_path):
-    todo = [{"Description": "Get some milk", "Priority": 2, "Done": False}]
+    todo = [{"Description": "Get some milk"}]
     db_file = tmp_path / "todo.json"
     with db_file.open("w") as db:
         json.dump(todo, db, indent=4)
@@ -27,38 +27,33 @@ def mock_json_file(tmp_path):
 
 test_data1 = {
     "description": ["Clean", "the", "house"],
-    "priority": 1,
     "todo": {
         "Title": "Clean the house",
-        "Priority": 1
     },
 }
 test_data2 = {
     "description": ["Wash the car"],
-    "priority": 2,
     "todo": {
         "Title": "Wash the car",
-        "Priority": 2
     },
 }
 
 @pytest.mark.parametrize(
-    "description, priority, expected",
+    "description, expected",
     [
         pytest.param(
             test_data1["description"],
-            test_data1["priority"],
             (test_data1["todo"], SUCCESS),
         ),
         pytest.param(
             test_data2["description"],
-            test_data2["priority"],
             (test_data2["todo"], SUCCESS),
         ),
     ],
 )
-def test_add(mock_json_file, description, priority, expected):
+def test_add(mock_json_file, description, expected):
     todoer = kanban.Kanban(mock_json_file)
-    assert todoer.add(description, priority) == expected
-    read = todoer._db_handler.read_todos()
-    assert len(read.todo_list) == 2
+    result = todoer.add(description, '')
+    assert result.card['Title'] == expected[0]['Title']
+    read = todoer._db_handler.read_cards()
+    assert len(read.card_list) == 2
