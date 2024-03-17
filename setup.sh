7d4e98c2e61767a15e37aee1f1656bb9a4677b8b
strikingLoo <lucianostrika44@gmail.com>
2024-03-17 16:03:40 -0700

    update setup commands

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export KTKB_PATH=$(pwd)
-python -m venv ./venv
+python3 -m venv ./venv
 source venv/bin/activate
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
